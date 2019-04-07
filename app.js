d5eb3957eb8e97fe584a5e51a120724e06a10f7c
Garrett Luu <garrettluu131@gmail.com>
2019-04-06 22:47:22 -0700

    Added server-side firebase support

--- app.js
+++ app.js
@@ -9,6 +9,21 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+//Initialize Firebase
+let firebase = require('firebase');
+var config = {
+    apiKey: "AIzaSyBxvyLamhcTZczjtobRuYFIE6RahuIV-mg",
+    authDomain: "carpool-1554591126344.firebaseapp.com",
+    databaseURL: "https://carpool-1554591126344.firebaseio.com",
+    projectId: "carpool-1554591126344",
+    storageBucket: "carpool-1554591126344.appspot.com",
+    messagingSenderId: "769999411158"
+};
+
+firebase.initializeApp(config);
+
+var database = firebase.database();
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
@@ -24,18 +39,18 @@ app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+    next(createError(404));
 });
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
 });
 
 module.exports = app;
