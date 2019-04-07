d33408dda72750d57c224ea8c4dcfea0792b0ec7
Brian <bleung@ucsd.edu>
2019-04-07 01:02:20 -0700

    Fixed title element

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Carpool: Redefining Ridesharing' });
+  res.render('index', { title: 'Carpool: Rideshare Redefined' });
 });
 
 module.exports = router;
