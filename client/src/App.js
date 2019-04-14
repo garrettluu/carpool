7cc96e991a97c382248196c336b587dde0cb96ea
Garrett Luu <garrettluu131@gmail.com>
2019-04-14 01:57:02 -0700

    Added parallax scrolling

--- client/src/App.js
+++ client/src/App.js
@@ -7,35 +7,40 @@ import Home from './Home';
 
 //CSS stuff
 import logo from './images/sports-car.svg';
-import './App.css';
+import './stylesheets/App.css';
+
+//Parallax scrolling
+import { ParallaxProvider } from 'react-scroll-parallax';
 
 class App extends Component {
     render() {
         return (
-            <div className="App">
-                <BrowserRouter>
-                    <div id="header">
-                        <div id="title-bar" className="nav-bar">
-                            <div className="nav-group">
-                                <NavLink id="company" className="nav-link" to="/"> Carpool </NavLink>
-                             i  <NavLink id="logo-link" to="/">
-                                    <img src={logo} height="60px" alt="logo">
-                                    </img>
-                                </NavLink>
-                                <NavLink id="rides-link" className="nav-link" to="/rides"> Rides </NavLink>
-                                <NavLink id="drive-link" className="nav-link" to="/drive"> Drive </NavLink>
-                            </div>
-                            <div class="nav-group">
-                                <NavLink id="signup-link" className="nav-link" to="/signup"> Sign Up </NavLink>
-                                <NavLink id="login-link" className="nav-link" to="/login"> Login </NavLink>
+            <ParallaxProvider>
+                <div className="App">
+                    <BrowserRouter>
+                        <div id="header">
+                            <div id="title-bar" className="nav-bar">
+                                <div className="nav-group">
+                                    <NavLink id="company" className="nav-link" to="/"> Carpool </NavLink>
+                                    <NavLink id="logo-link" to="/">
+                                        <img src={logo} height="60px" alt="logo">
+                                        </img>
+                                    </NavLink>
+                                    <NavLink id="rides-link" className="nav-link" to="/rides"> Rides </NavLink>
+                                    <NavLink id="drive-link" className="nav-link" to="/drive"> Drive </NavLink>
+                                </div>
+                                <div class="nav-group">
+                                    <NavLink id="signup-link" className="nav-link" to="/signup"> Sign Up </NavLink>
+                                    <NavLink id="login-link" className="nav-link" to="/login"> Login </NavLink>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                    <div className="content">
-                        <Route exact path="/" component={Home}/>
-                    </div>
-                </BrowserRouter>
-            </div>
+                        <div className="content">
+                            <Route exact path="/" component={Home}/>
+                        </div>
+                    </BrowserRouter>
+                </div>
+            </ParallaxProvider>
         );
     }
 }
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,38 +1,57 @@
 import React, { Component } from 'react';
+import { Parallax } from 'react-scroll-parallax';
 
-import './Home.css';
+import './stylesheets/Home.css';
+
+const ParallaxImage = () => (
+    <Parallax y={[-50, 25]}>
+        <img src="./images/long-road.jpg"/>
+    </Parallax>
+);
 
 class Home extends Component {
     render() {
         return (
             <div className="Home">
-                <div id="ridesharing-redefined">
-                    <p id="pitch-tag1">Ridesharing.</p>
-                    <p id="pitch-tag2">Redefined.</p>
-                </div>
+                <Parallax y={[-100, 64]}>
+                    <div id="ridesharing-redefined">
+                        <p id="pitch-tag1">Ridesharing.</p>
+                        <p id="pitch-tag2">Redefined.</p>
+                    </div>
+                </Parallax>
                 <div id="welcome-messages">
-                    <div>
-                        <h3 class="titles-home" id="introduction">
+                    <div className="welcome-message">
+                        <h3 className="titles-home" id="introduction">
                             The fastest way to get a ride.
                         </h3>
-                        <p class="descriptions" id="about">
+                        <p className="descriptions" id="about">
+                            Carpool is the fastest, cheapest way to get to your destination.
                             Just enter where you want to go, and it will find nearest cars available to you, updated in real time.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
                         </p>
                     </div>
 
-                    <h3 class="titles-home">
-                        Anyone can sign up, anyone can become a driver!
-                    </h3>
-                    <p class="descriptions">
-                        You just need to set up an account with your information and you can start driving!
-                    </p>
+                    <div className="welcome-message">
+                        <h3 className="titles-home">
+                            Decentralized driving.
+                        </h3>
+                        <p className="descriptions">
+                            Anyone can sign up and become a driver.
+                            You just need to set up an account with your information and you can start driving!
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
 
-                    <h3 class="titles-home">
-                        To ride, you need credits, which can be earned by being a driver yourself!
-                    </h3>
-                    <p class="descriptions">
-                        We do this to incentivize a community of ridesharers and contributers to the community.
-                    </p>
+                    <div className="welcome-message">
+                        <h3 className="titles-home">
+                            Earn credits by driving.
+                        </h3>
+                        <p className="descriptions">
+                            By driving others, you can earn credits, which can be used for free rides from other drivers.
+                            We do this to incentivize a community of ridesharers and contributors to the community.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                        </p>
+                    </div>
                 </div>
             </div>
         );
