245dcd9caf5b3504bc2811ff9a8f86374aaea6dc
Garrett Luu <garrettluu131@gmail.com>
2019-04-13 20:17:55 -0700

    Made nav bar fully react

--- client/src/App.js
+++ client/src/App.js
@@ -3,28 +3,30 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        /*
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        */
-      </div>
-    );
-  }
-}
+    render() {
+        return (
+            <div className="App">
+                <div id="header">
+                    <div id="title-bar" class="nav-bar">
+                        <div class="nav-group">
+                            <a id="company" class="nav-link" href = "/"> Carpool </a>
+                            <a id="logo-link" href="/">
+                                <img src="./images/sports-car.svg" height="60px" >
+                                </img>
+                            </a>
+                            <a id="rides-link" class="nav-link" href="/rides"> Rides </a>
+                            <a id="drive-link" class="nav-link" href = "/drive"> Drive </a>
+                        </div>
+                        <div class="nav-group">
+                            <a id="signup-link" class="nav-link" href = "/signup"> Sign Up </a>
+                            <a id="login-link" class="nav-link" href = "/login"> Login </a>
+                        </div>
+                    </div>
+                </div>
+                <div className="content">
+                </div>
+            </div>);
+        }
+    }
 
-export default App;
+    export default App;
