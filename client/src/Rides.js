b6587fa7b7cf524409c46604589e442ab7cd10a2
Garrett Luu <garrettluu131@gmail.com>
2019-04-15 21:03:07 -0700

    Ported rides page to react

--- client/src/Rides.js
+++ client/src/Rides.js
@@ -1,6 +1,55 @@
 import React, {Component} from 'react';
+import DriverCard from './components/DriverCard';
+import axios from 'axios';
 
 class Rides extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {
+            data: {}
+        };
+        this.getData = this.getData.bind(this);
+    }
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData() {
+        axios.get('http://localhost:3001/update')
+            .then((response) => {
+                this.setState({
+                    data: response.data
+                });
+            });
+    }
+
+    static newDriverCard(name, seats) {
+        return (
+            <DriverCard name={name}
+                        seats={seats}/>
+        )
+    }
+
+    buildDriverCardsFromDatabase() {
+        let data = this.state.data;
+        let cards = [];
+        for (let key in data) {
+            if (data.hasOwnProperty(key)) {
+                cards.push(Rides.newDriverCard(data[key].name, data[key].seats));
+            }
+        }
+        return cards;
+    }
+
     render() {
+        return (
+            <div className="driver-container">
+                {this.buildDriverCardsFromDatabase()}
+            </div>
+        );
     }
 }
+
+export default Rides;
